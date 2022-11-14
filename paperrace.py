c726d8f6eb7844cdbafd7db6189e5521d8da9fe7
Bodo <bodo.akdeniz@gmail.com>
2022-11-14 13:40:29 +0100

    Add different agents to test

--- paperrace.py
+++ paperrace.py
@@ -1,17 +1,21 @@
 from game.pygletgui import Main
 from game.gamestate import PaperRaceGameState
-from game.agent import PRAgent, SimplePRAgent, DFSPRAgent, BetterPRAgent, AStarPRAgent, SimplePRAgent2
+from game.agent import SimplePRAgent, SimplePRAgent2
 
 
 if __name__ == '__main__':
     gamestate = PaperRaceGameState()
-    gamestate.load_map("maps/map4.ini", 2)
-    #agent1 = SimplePRAgent(gamestate, 0)
-    agent2 = SimplePRAgent2(gamestate, 1)
+    gamestate.load_map("maps/map10.ini", 4)
+    agent1 = SimplePRAgent2(gamestate, 0, 5)
+    agent2 = SimplePRAgent2(gamestate, 1, 3)
+    agent3 = SimplePRAgent2(gamestate, 2, 1)
+    agent4 = SimplePRAgent(gamestate, 3)
     
     main = Main(gamestate)
-    #main.add_agent(agent1, agent1.racer_id)
+    main.add_agent(agent1, agent1.racer_id)
     main.add_agent(agent2, agent2.racer_id)
+    main.add_agent(agent3, agent3.racer_id)
+    main.add_agent(agent4, agent4.racer_id)
 
     main.run()
 
